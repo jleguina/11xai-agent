8743966b1a168838c2ecd26f9d389c0d41796a3c
Javier Leguina <javier@leguina.com>
2023-10-22 17:30:01 +0100

    Cleanup

--- app/integrations/gcal.py
+++ app/integrations/gcal.py
@@ -1,39 +1,10 @@
 import datetime
-import os
-import pickle
 from typing import Any
 
-from google.auth.transport.requests import Request
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
 from app.config import settings
 from app.integrations.google_auth import GoogleService, get_google_service
-
-
-def gcal_authenticate(
-    client_config: dict[str, dict[str, str | list[str]]],
-    scopes: list[str],
-) -> Any:
-    creds = None
-    # the file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first time
-    if os.path.exists("token.pickle"):
-        with open("token.pickle", "rb") as token:
-            creds = pickle.load(token)
-
-    # if there are no (valid) credentials availablle, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_config(client_config, scopes)
-            creds = flow.run_local_server(port=0)
-        # save the credentials for the next run
-        with open("token.pickle", "wb") as token:
-            pickle.dump(creds, token)
-    return build("calendar", "v3", credentials=creds)
 
 
 def test_gcal(service: Any) -> None:
